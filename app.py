bee4e5499d599152778f77ac6d608314797312ac
pabbidi7-lgtm <pabbidi.venkata2024@vitstudent.ac.in>
2026-03-26 20:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-sudo docker compose up -d
-[sudo] password for clouduser01: 
-WARN[0000] /home/clouduser01/jaswanth/docker-compose.yml: the attribute `version` is obsolete, it will be ignored, please remove it to avoid potential confusion 
-[+] up 2/3
- ! Image redis:7                                        Interrupted                                       1.8s 
- ⠇ Image milvusdb/milvus:v2.4.4                         Pulling                                           1.8s 
- ✘ Image nvcr.io/nvidia/nemo-retriever/nv-ingest:25.9.0 Error error from registry: Access Denied          1.8s 
-Error response from daemon: error from registry: Access Denied
-(myenv) clouduser01@AZRCIDEVNIVIDIA:~/jaswanth$ sudo docker login nvcr.io
-Authenticating with existing credentials... [Username: $oauthtoken]
-
-i Info → To login with a different account, run 'docker logout' followed by 'docker login'
-
-
-Login Succeeded
-(myenv) clouduser01@AZRCIDEVNIVIDIA:~/jaswanth$ 
+taskset -c 0-7 python pipeline.py
+2026-03-26 14:38:01.792540856 [W:onnxruntime:Default, device_discovery.cc:132 GetPciBusId] Skipping pci_bus_id for PCI path at "/sys/devices/LNXSYSTM:00/LNXSYBUS:00/ACPI0004:00/VMBUS:00/5620e0c7-8062-4dce-aeb7-520c7ef76171" because filename ""5620e0c7-8062-4dce-aeb7-520c7ef76171"" dit not match expected pattern of [0-9a-f]+:[0-9a-f]+:[0-9a-f]+[.][0-9a-f]+
+INFO:nv_ingest_api.util.system.hardware_info:Detected 32 logical cores via psutil.
+INFO:nv_ingest_api.util.system.hardware_info:Detected 16 physical cores via psutil.
+INFO:nv_ingest_api.util.system.hardware_info:Detected 8 cores via os.sched_getaffinity.
+INFO:nv_ingest_api.util.system.hardware_info:Raw CPU limit determined: 8.00 (Method: sched_affinity)
+INFO:nv_ingest_api.util.system.hardware_info:Effective CPU core limit determined: 8.00 (Method: sched_affinity)
+INFO:nv_ingest.framework.orchestration.ray.util.pipeline.pipeline_runners:Launching pipeline in Python subprocess using multiprocessing.
+INFO:nv_ingest.framework.orchestration.ray.util.pipeline.pipeline_runners:Pipeline subprocess started (PID=2192257)
+Starting ingestion..
+Processing:   0%|                                                                            | 0/1 [00:00<?, ?doc/s]INFO:nv_ingest_client.client.client:Starting batch processing for 1 jobs with batch size 32.
+Processing: 100%|████████████████████████████████████████████████████████████████████| 1/1 [01:11<00:00, 71.21s/doc]INFO:nv_ingest_client.client.client:Batch processing finished. Success: 1, Failures: 0. Total accounted for: 1/1
+Processing: 100%|████████████████████████████████████████████████████████████████████| 1/1 [01:11<00:00, 71.21s/doc]
+WARNING: All log messages before absl::InitializeLog() is called are written to STDERR
+E0000 00:00:1774535954.976942 2192153 dns_resolver_ares.cc:358] no server name supplied in dns URI
+E0000 00:00:1774535954.977023 2192153 legacy_channel.cc:89] channel stack builder failed: UNKNOWN: the target uri is not valid: dns:///
