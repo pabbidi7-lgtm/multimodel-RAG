9106bdd56259415d12c18dab85617cf1b19cc7be
pabbidi7-lgtm <pabbidi.venkata2024@vitstudent.ac.in>
2026-03-26 20:26:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
-taskset -c 0-7 python pipeline.py
-2026-03-26 14:38:01.792540856 [W:onnxruntime:Default, device_discovery.cc:132 GetPciBusId] Skipping pci_bus_id for PCI path at "/sys/devices/LNXSYSTM:00/LNXSYBUS:00/ACPI0004:00/VMBUS:00/5620e0c7-8062-4dce-aeb7-520c7ef76171" because filename ""5620e0c7-8062-4dce-aeb7-520c7ef76171"" dit not match expected pattern of [0-9a-f]+:[0-9a-f]+:[0-9a-f]+[.][0-9a-f]+
-INFO:nv_ingest_api.util.system.hardware_info:Detected 32 logical cores via psutil.
-INFO:nv_ingest_api.util.system.hardware_info:Detected 16 physical cores via psutil.
-INFO:nv_ingest_api.util.system.hardware_info:Detected 8 cores via os.sched_getaffinity.
-INFO:nv_ingest_api.util.system.hardware_info:Raw CPU limit determined: 8.00 (Method: sched_affinity)
-INFO:nv_ingest_api.util.system.hardware_info:Effective CPU core limit determined: 8.00 (Method: sched_affinity)
-INFO:nv_ingest.framework.orchestration.ray.util.pipeline.pipeline_runners:Launching pipeline in Python subprocess using multiprocessing.
-INFO:nv_ingest.framework.orchestration.ray.util.pipeline.pipeline_runners:Pipeline subprocess started (PID=2192257)
-Starting ingestion..
-Processing:   0%|                                                                            | 0/1 [00:00<?, ?doc/s]INFO:nv_ingest_client.client.client:Starting batch processing for 1 jobs with batch size 32.
-Processing: 100%|████████████████████████████████████████████████████████████████████| 1/1 [01:11<00:00, 71.21s/doc]INFO:nv_ingest_client.client.client:Batch processing finished. Success: 1, Failures: 0. Total accounted for: 1/1
-Processing: 100%|████████████████████████████████████████████████████████████████████| 1/1 [01:11<00:00, 71.21s/doc]
-WARNING: All log messages before absl::InitializeLog() is called are written to STDERR
-E0000 00:00:1774535954.976942 2192153 dns_resolver_ares.cc:358] no server name supplied in dns URI
-E0000 00:00:1774535954.977023 2192153 legacy_channel.cc:89] channel stack builder failed: UNKNOWN: the target uri is not valid: dns:///
-
-
-
 curl -X POST https://ai.api.nvidia.com/v1/retrieval/nvidia/embeddings \
   -H "Authorization: Bearer $NVIDIA_API_KEY" \
   -H "Content-Type: application/json" \
   -d '{"input": ["test"], "model": "nvidia/nv-embedqa-e5-v5", "input_type": "query"}' \
   | python3 -m json.tool
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100   240  100   162  100    78    504    243 --:--:-- --:--:-- --:--:--   750
+{
+    "status": 404,
+    "title": "Not Found",
+    "detail": "Function '09c64e32-2b65-4892-a285-2f585408d118': Not found for account '38FN-XDcco9msc2defg-jUydEzvtOLO0B4O0UlrcXzw'"
+}
